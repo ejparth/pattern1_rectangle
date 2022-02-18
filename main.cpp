55f6821750752471e9445658fec18395eb91881c
aakash saxena <aakash.saxena08@gmail.com>
2022-02-18 19:03:02 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+//pattern to print rectangle
+
+  for(int i = 0; i<5; i++)
+  {
+    
+    for(int i = 0; i<5; i++)
+      {
+        cout << "*";
+      }
+        cout <<"\n";
+  }
+
+return 0;
+}
